0b2e06d9527db4cbbd0a3fc4fd41884a69a9f487
Hassan Abid <hassanabidpk89@gmail.com>
2016-02-13 19:07:32 +0900

    add home page

--- web/scantact/scantact/settings.py
+++ web/scantact/scantact/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'userprofile',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -118,4 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
--- web/scantact/scantact/urls.py
+++ web/scantact/scantact/urls.py
@@ -13,10 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns, static
+from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
 	url(r'^',include('userprofile.urls', namespace='userprofile')),
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
